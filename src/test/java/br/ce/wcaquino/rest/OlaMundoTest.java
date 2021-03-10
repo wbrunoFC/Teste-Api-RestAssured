281f3ed50d2b8ed3b5e2707356873a9117664b90
wallison.ferreira <wallison.ferreira@basis.com.br>
2021-03-10 14:33:48 -0300

    Validacoes Avancadas

--- src/test/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ src/test/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -70,7 +70,7 @@ public class OlaMundoTest {
 		assertThat("Jose", anyOf(is("Jose"), is("Maria")));
 		
 		// Verifique se o nome comeca com... && temina com.... && contem...
-		assertThat("Joaquina", allOf(startsWith("m"), endsWith("ia"), containsString("ar")));
+		assertThat("Joaquina", allOf(startsWith("J"), endsWith("na"), containsString("oaqui")));
 		
 	}
 	
@@ -82,9 +82,9 @@ public class OlaMundoTest {
 		.then().
 			statusCode(200)
 			// Verificacao rigida com toda a resposta
-			.body(is("Ola, Mundo!"))
+			.body(is("Ola Mundo!"))
 			// Verificacao flexivel
-			.body(contains("Ola"))
+			.body(containsString("Ola"))
 			// Verificacao generica (Verifique se a respota nao esta vazia)
 			.body(is(not(nullValue())));
 		
@@ -160,6 +160,91 @@ public class OlaMundoTest {
 		.then()
 			.statusCode(404)
 			.body("error", is("Usuário inexistente"));
+	}
+	
+	@Test
+	public void devoVerificarListaRaiz() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			
+			// Verifica se na raiz tem 3 objetos ($ convencao da raiz)
+			.body("$", hasSize(3))
+			
+			// Verifica se na lista tem os nome...
+			.body("name", hasItems("João da Silva", "Ana Júlia"))
+			
+			// Verifica se tem uma idade igual a 25
+			.body("age[1]", is(25))
+			
+			
+			// Verifica se na lista possui outra lista com os nomes...
+			.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+			
+			
+			.body("salary", contains(1234.5678f, 2500, null));
+	}
+	
+	@Test
+	public void deveFazerVerificacoesAvencadas() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			
+			// Verifica se na raiz tem 3 objetos ($ convencao da raiz)
+			.body("$", hasSize(3))
+			
+			// Verifica se na lista tem idade duas idades menor igual a...
+			.body("age.findAll{it <= 25}.size()", is(2))
+			
+			// Verifica se na lsita tem uma idade idade menor que... e maior que...
+			.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+			
+			// Verifica se na lsita tem idade menor que... e maior que... buscando o nome. 
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			
+			// Verifica se na lista raiz se tem na primeira posicao alguem com idade menor igual a... buscando o nome
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			
+			// Verifica se na lista raiz se tem na primeira posicao do ultimo item ao primeiro, alguem com idade menor igual a... buscando o nome
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			
+			// Verifique se tem uma pessoa com idade menor igual a ... buscando nome
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			
+			// Verifique se tem pessoas duas pessoas com n no nome
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+			
+			// Verifique nomes dois nome que sao maiores que 10 caracateres
+			.body("findAll{it.name.length() > 10}.name", hasItems("Maria Joaquina", "João da Silva"))
+			
+			// Verifique na lista um nome e transforme para maiusculo
+			.body("name.collect{it.toUpperCase()}", hasItems("MARIA JOAQUINA"))
+			
+			// Verifique um nome que comeca com Maria e transforme para maiusculo
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			
+			// Verifique um nome que comeca com Maria e transforme para maiusculo e o tamanho do array eh um
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+			
+			// Veririque as idades e multiplique por 2
+			.body("age.collect{it * 2}", hasItems(60, 50, 40))
+			
+			// Verifique o id maximo na lista
+			.body("id.max()", is(3))
+			
+			// Verifique o menor salario da lista
+			.body("salary.min()", is(1234.5678f))
+			
+			// Verifique os salarios e some eles com uma margem de erro de 0.001
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			
+			// Verique todos os salarios e some, devendo ser maior que 3000 e menor que 5000
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)));
 	}
 
 }
